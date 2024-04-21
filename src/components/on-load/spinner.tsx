04762f07b206aa5db2bda335f4255b2973273a2f
Cockamamie <bugrymenko@yandex.ru>
2024-04-21 13:41:36 +0500

    Replace loading spinner

--- src/components/on-load/spinner.tsx
+++ src/components/on-load/spinner.tsx
@@ -1,11 +1,24 @@
-import {JSX} from 'react';
-import './spinner.css';
+import {CSSProperties, JSX} from 'react';
+import {InfinitySpin} from 'react-loader-spinner';
 
 export default function Spinner(): JSX.Element {
+  const wrapperStyle: CSSProperties = {
+    position: 'relative',
+    left: '50%'
+  };
+  const containerStyle: CSSProperties = {
+    position: 'absolute',
+    transform: 'translate(-50%, 0)'
+  };
+
   return (
-    <div className="lds-ripple">
-      <div></div>
-      <div></div>
+    <div className='cities__spinner-wrapper' style={wrapperStyle}>
+      <div className='cities__spinner-container' style={containerStyle}>
+        <InfinitySpin
+          width="200"
+          color="#4481c3"
+        />
+      </div>
     </div>
   );
 }
