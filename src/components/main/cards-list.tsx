af370b7993933bedfc6a8b0380c35714c0b03b61
Cockamamie <bugrymenko@yandex.ru>
2024-04-30 19:23:32 +0500

    M6T2 Implement cards sort

--- src/components/main/cards-list.tsx
+++ src/components/main/cards-list.tsx
@@ -4,19 +4,37 @@ import Card from './card.tsx';
 import Location from '../../types/location.ts';
 import {useAppSelector} from '../../hooks';
 import Spinner from '../on-load/spinner.tsx';
+import {SortOrder, SortType} from '../../types/sort.ts';
 
 type Props = {
   cards: CardInfo[];
   onListItemHover: (listPoint: Location) => void;
   listClassNames: string;
+  sortType: SortType;
+  sortOrder: SortOrder;
 }
 
-export default function CardsList({cards, onListItemHover, listClassNames}: Props): JSX.Element {
+const sortCards = (cards: CardInfo[], type: SortType, order: SortOrder) => {
+  if (type === SortType.Price) {
+    const compareFunction: (a: CardInfo, b: CardInfo) => number = order === SortOrder.Descending
+      ? (a, b) => b.price - a.price
+      : (a, b) => a.price - b.price;
+    return cards.toSorted(compareFunction);
+  }
+
+  if (type === SortType.Rating && order === SortOrder.Descending) {
+    return cards.toSorted((a, b) => b.rating - a.rating);
+  }
+
+  return cards;
+};
+
+export default function CardsList({cards, onListItemHover, listClassNames, sortType, sortOrder}: Props): JSX.Element {
   const isLoading = useAppSelector((state) => state.isLoading);
 
   const createCards = () => (
     <>
-      {cards.map((card) =>
+      {sortCards(cards, sortType, sortOrder).map((card) =>
         <Card key={card.id} card={card} onListItemHover={onListItemHover} classPrefix={'cities'}/>)}
     </>
   );
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,6 +5,9 @@ import Location from '../../types/location.ts';
 import {useAppSelector} from '../../hooks';
 import Tabs from './tabs.tsx';
 import {cities} from '../../consts.ts';
+import Sort from './sort.tsx';
+import {useSearchParams} from 'react-router-dom';
+import {SortOrder, SortType} from '../../types/sort.ts';
 
 // TODO: fix main page scroll
 // TODO: fix slash between price and period
@@ -12,6 +15,9 @@ export default function Main(): JSX.Element {
   // TODO: city resets on page refresh
   const [city, allCards] = useAppSelector((state) => [state.city, state.offers]);
   const [selectedPoint, setSelectedPoint] = useState<Location | undefined>(undefined);
+  const [searchParams] = useSearchParams();
+  const sortType = searchParams.get('sort') ?? SortType.Popularity;
+  const sortOrder = searchParams.get('order') ?? SortOrder.Descending;
 
   const cards = allCards.filter((card) => card.city.name === city.name);
   const points = cards.map((card) => card.location);
@@ -35,25 +41,13 @@ export default function Main(): JSX.Element {
               <b className="places__found">
                 {cards.length} {cards.length === 1 ? 'place' : 'places'} to stay in {city.name}
               </b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select"/>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <Sort />
               <CardsList
                 cards={cards}
                 onListItemHover={handleListItemHover}
                 listClassNames={'cities__places-list places__list tabs__content'}
+                sortType={sortType as SortType}
+                sortOrder={sortOrder as SortOrder}
               />
             </section>
             <div className="cities__right-section">
